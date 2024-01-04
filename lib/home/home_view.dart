c38e381d2698ae616c236c11966583242cbfaa33
Alperen Göyce <goycea@gmail.com>
2024-01-04 23:51:17 +0300

    Updated App bar
    Updated Home View App bar

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -63,6 +63,7 @@ class _HomeViewState extends State<HomeView> with Base {
       appBar: AppBar(
         title: const Text("Barcode Generator"),
         backgroundColor: colorConstant.firstGradient,
+        centerTitle: true,
       ),
       bottomNavigationBar: Container(
         height: context.dynamicHeight(0.08),
